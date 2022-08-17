acdbd79ee8ec597c8ec797caeb9a3b34273d72be
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-08-17 19:52:25 +0400

    Search Algo Done

--- js/app.js
+++ js/app.js
@@ -1,24 +1,83 @@
 // Search form
 const searchBTN = document.querySelector(".searchBTN");
 const form = document.querySelector(".form");
+const checkInputs = document.querySelectorAll(".checkInput");
 const checkText = `
                 <span class="checkText">This field is required</span>
                 `;
-searchBTN.addEventListener("click", function salamla(e) {
+const checkTextFormat = `
+                <span class="checkText">Format is not valid</span>
+                `;
+const time = new Date();
+// Search Availabilty function start
+searchBTN.addEventListener("click", function searchAvailaility(e) {
   e.preventDefault();
-  //   console.log(form.children);
-  for (let input of form.children) {
-    // console.log(input.lastElementChild.tagName);
-
-    // console.log(input.children[1]);
+  const day = time.getDate() + time.getMonth() + 1 + time.getFullYear();
+  for (let inputs of checkInputs) {
     if (
-      input.lastElementChild.tagName == "INPUT" &&
-      input.lastElementChild.value == ""
+      inputs.value.length < 8 ||
+      inputs.value.length > 8 ||
+      !Number(inputs.value)
     ) {
-      input.innerHTML += checkText;
-      console.log("salam");
+      if (inputs.value == "") {
+        if (!inputs.nextElementSibling) {
+          inputs.insertAdjacentHTML("afterend", checkText);
+        } else if (
+          inputs.nextElementSibling.innerHTML == "Format is not valid"
+        ) {
+          inputs.nextElementSibling.innerHTML = "This field is required";
+        }
+      } else {
+        inputs.insertAdjacentHTML("afterend", checkTextFormat);
+        inputs.value = "";
+      }
+    } else if (inputs.value.slice(4, 8) < time.getFullYear()) {
+      inputs.insertAdjacentHTML("afterend", checkTextFormat);
+      inputs.value = "";
+    } else if (inputs.value.slice(4, 8) == time.getFullYear()) {
+      if (
+        inputs.value.slice(2, 4) > 12 ||
+        inputs.value.slice(2, 4) <= time.getMonth() + 1
+      ) {
+        if (
+          inputs.value.slice(2, 4) == time.getMonth() + 1 ||
+          (inputs.value.slice(2, 4) < time.getMonth() + 1 &&
+            (inputs.value.slice(0, 2) > 31 ||
+              inputs.value.slice(0, 2) < time.getDate()))
+        ) {
+          inputs.insertAdjacentHTML("afterend", checkTextFormat);
+          inputs.value = "";
+        }
+      } else {
+        if (
+          inputs.value.slice(0, 2) > 31 ||
+          inputs.value.slice(0, 2) < time.getDate()
+        ) {
+          inputs.insertAdjacentHTML("afterend", checkTextFormat);
+          inputs.value = "";
+        }
+      }
     }
   }
 });
+// End Search Availabilty function start
+// Input Error Delete Function Start
+function deleteError(e) {
+  if (e.nextElementSibling) {
+    e.nextElementSibling.remove();
+  }
+}
 
-// End Search form
+// // End Search form
+// //
+// All form check
+// for (let input of form.children) {
+//   if (
+//     input.lastElementChild.tagName == "INPUT" &&
+//     input.lastElementChild.value == ""
+//   ) {
+//     input.innerHTML += checkText;
+//     console.log("salam", input);
+//   }
+// }
+// Check in/out input format check
